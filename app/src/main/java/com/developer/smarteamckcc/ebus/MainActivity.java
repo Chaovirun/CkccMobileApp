48c7575a791f6130a4b2d5a1b4da65f6d8891dbf
chaovirun <choavirun092@gmail.com>
2018-05-24 18:11:02 +0700

    [My project eBus]

---
+++ app/src/main/java/com/developer/smarteamckcc/ebus/MainActivity.java
@@ -0,0 +1,20 @@
+package com.developer.smarteamckcc.ebus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvCreate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+
+    }
+}
